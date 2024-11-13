311cf43db02b958747b84f01e46f37cbea942631
anderson <andersondjame@gmail.com>
2024-11-13 22:12:17 +0000

    ajout de gitignore, de mysql et env

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const express =require("express")
+//declare express
+const express = require("express")
+//declare dotenv - utilisation des configuration dotenv
+require('dotenv').config();
+//declare mysql
+const mysql = require("mysql")
+//declare port
 const port = 5000
+
+//init express on const app
 const app = express()
 
+//init connexion
+const database= mysql.createConnection({
+    host:process.env.DBHOST,
+    user:process.env.DBUSER,
+    database:process.env.DBNAME,
+    password:process.env.DBPASSWORD
+})
 
-app.get("/",(req,res)=>{
+database.connect((err)=>{
+    if(err){
+        console.error("Erreur"+err.stack)
+        return
+    }
+    console.log("Connexion réussie")
+})
+
+app.get("/", (req, res) => {
     //res.json({"message":"bonjour"})
     res.status(200).send("hello andy")
 })
 const users = require("./routes/users")
-app.use("/users",users)
+app.use("/users", users)
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("server run...")
 })
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const router = express.Router()
-
+require('dotenv').config();
+//declare mysql
+const mysql = require("mysql")
+//init connexion
+const database= mysql.createConnection({
+    host:process.env.DBHOST,
+    user:process.env.DBUSER,
+    database:process.env.DBNAME,
+    password:process.env.DBPASSWORD
+})
 router.get("/",(req,res)=>{
-    res.status(200).send("Hello user")
+    database.query("SELECT * FROM user",(err,row,field)=>{
+        if(err) throw err;
+        res.json(row)
+    })
 })
 
-module.exports=router
+module.exports = router
